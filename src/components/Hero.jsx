fa7d343caf9cb1818d1e116b751dadb533c5846b
Tuan Phung <16448321+tuanphungcz@users.noreply.github.com>
2022-12-27 02:16:59 -0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,10 +8,10 @@ import { Button } from './common';
 const highlitedContent = `
 import React from "react";
 import UseAnimations from "react-useanimations";
-import github from 'react-useanimations/lib/github'
+import github from 'react-useanimations/lib/github';
 
 const App = () => (
-  <UseAnimations animation={github} size={56} style={{ padding: 100 }} />
+  <UseAnimations animation={github} size={56} />
 );
 
 `;
